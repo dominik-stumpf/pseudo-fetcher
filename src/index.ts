5e838a3aa5e8f12942e6628949aad9cc14767df6
Dominik Stumpf <s.dominik.work@gmail.com>
2023-10-14 12:34:48 +0200

    added biome, formatted project

--- src/index.ts
+++ src/index.ts
@@ -1,45 +1,45 @@
-import { readFileSync } from "fs";
-import path from "path";
+import { readFileSync } from 'fs';
+import path from 'path';
 
 function readFile(relativePathFromRoot: string) {
-	const filePath = path.resolve(relativePathFromRoot);
-	const data = readFileSync(filePath, "utf-8");
-	return data;
+  const filePath = path.resolve(relativePathFromRoot);
+  const data = readFileSync(filePath, 'utf-8');
+  return data;
 }
 
 function parseHeaders(rawHeaders: string, startFromLine = 1) {
-	const filteredHeaders = ["Content-Length"];
+  const filteredHeaders = ['Content-Length'];
 
-	return Object.fromEntries(
-		rawHeaders
-			.split("\n")
-			.slice(startFromLine)
-			.filter((key) => !filteredHeaders.every((filter) => key.includes(filter)))
-			.map((line) => {
-				const colonIndex = line.indexOf(":");
-				return [
-					line.slice(0, colonIndex).trim(),
-					line.slice(colonIndex + 1).trim(),
-				];
-			}),
-	);
+  return Object.fromEntries(
+    rawHeaders
+      .split('\n')
+      .slice(startFromLine)
+      .filter((key) => !filteredHeaders.every((filter) => key.includes(filter)))
+      .map((line) => {
+        const colonIndex = line.indexOf(':');
+        return [
+          line.slice(0, colonIndex).trim(),
+          line.slice(colonIndex + 1).trim(),
+        ];
+      }),
+  );
 }
 
 async function main() {
-	const parsedHeaders = parseHeaders(readFile("./request/headers"));
-	const payload = readFile("./request/payload");
-	const api = readFile("./request/api");
+  const parsedHeaders = parseHeaders(readFile('./request/headers'));
+  const payload = readFile('./request/payload');
+  const api = readFile('./request/api');
 
-	const headers = new Headers(parsedHeaders);
-	const request: RequestInit = {
-		headers: headers,
-		body: payload,
-		method: "POST",
-	};
-	const res = await fetch(api, request);
+  const headers = new Headers(parsedHeaders);
+  const request: RequestInit = {
+    headers: headers,
+    body: payload,
+    method: 'POST',
+  };
+  const res = await fetch(api, request);
 
-	console.log(JSON.stringify(await res.json()));
-	console.log("fin", res.status, res.statusText);
+  console.log(JSON.stringify(await res.json()));
+  console.log('fin', res.status, res.statusText);
 }
 
 main();
